15b5bc40f05bb5f70675ef7ed57b03f5e0bb762e
LittleBiber <rlatkdgud98@naver.com>
2022-05-20 14:34:03 +0900

    Map 구현 시도 #3

--- src/components/Map/Canvas/index.js
+++ src/components/Map/Canvas/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, forwardRef } from "react";
 import * as PIXI from "pixi.js";
 import { Viewport } from "pixi-viewport";
 import styled from "styled-components";
@@ -30,11 +30,7 @@ export default function Canvas({
   nowSpot,
   setNowSpot,
 }) {
-  const canvasParent = useRef(null);
-  const [welcomeModal, setWelcomeModal] = useState(true);
-  const [spotModal, setSpotModal] = useState(false);
-  const [detailData, setDetailData] = useState({});
-
+  //! 고정으로 사용하는 값 선언
   const CANVAS_SETTING = {
     width: window.innerWidth, // vw
     height: window.innerHeight, // vh
@@ -42,9 +38,7 @@ export default function Canvas({
     backgroundColor: 0x4284d2,
   };
   const BACKGROUND_SIZE = { width: 1720, height: 1738 };
-
   const SECTOR_SIZE = 56 * 2;
-
   const UNLOCKED_SECTOR = {
     "3_9": 0,
     "4_9": 0,
@@ -64,17 +58,42 @@ export default function Canvas({
     "8_8": 0,
     "8_9": 0,
     "8_10": 0,
-  }; // 4th phase (final)
+  };
   const FIRST_FOCUS_SECTOR = "6_9";
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  // Blinking Effect
+  var blinkingGizmo = new PIXI.Graphics();
+  blinkingGizmo.zOrder = 10000;
+  blinkingGizmo.interactive = false;
+  blinkingGizmo.buttonMode = false;
+  // Attach spots
+  let buttons = new PIXI.Container();
+  let spotDict = {};
+  let sectorSpotDict = {};
+  let sectorDict = {};
+  let lastPopup = null;
 
+  //! 상태값 선언
+  const canvasParent = useRef(document.getElementById("test"));
+  console.log("선언 바로 뒤에", canvasParent);
+  const [welcomeModal, setWelcomeModal] = useState(true);
+  const [spotModal, setSpotModal] = useState(false);
+  const [detailData, setDetailData] = useState({});
+
+  //! 테스트로 사용하는 값 선언
+  let lastSector = null;
+
+  //! 코드 작성 시작
   const app = new PIXI.Application(CANVAS_SETTING);
-  useEffect(() => {
-    console.log("spot/spotDict", spotDict);
-    console.log("spot/sectorSpotDict", sectorSpotDict);
-    console.log("spot/sectorDict", sectorDict);
 
-    if (canvasParent.current) canvasParent.current.appendChild(app.view); // DOM접근 대신 useRef 사용
+  useEffect(() => {
+    // console.log("spot/spotDict", spotDict);
+    // console.log("spot/sectorSpotDict", sectorSpotDict);
+    // console.log("spot/sectorDict", sectorDict);
+    console.log("값이 바뀌나?", canvasParent.current);
+
+    if (canvasParent.current) canvasParent.current.appendChild(app.view);
+    mountCanvas(app, canvasParent);
   }, []);
 
   const viewport = new Viewport({
@@ -89,7 +108,6 @@ export default function Canvas({
     .drag()
     .pinch()
     .wheel()
-    // .decelerate()
     .clamp({
       left: -BACKGROUND_SIZE.width,
       right: BACKGROUND_SIZE.width * 2,
@@ -107,6 +125,11 @@ export default function Canvas({
     removePopup();
   });
   viewport.on("clicked", (event) => {
+    console.log("--------------------------------------------");
+
+    console.log("클릭이벤트 발생");
+    console.log("App 어떻게 나오나 체크", app);
+
     let sectorId = "";
     let farmInfo = null;
     let sector = null;
@@ -120,7 +143,10 @@ export default function Canvas({
       }
     }
 
-    if (sectorId != "") {
+    console.log("sector 찾은 값", sector);
+    console.log("sectorId", sectorId);
+
+    if (sectorId !== "") {
       let spots = sectorSpotDict[sectorId];
       for (let i in spots) {
         let s = spots[i];
@@ -132,26 +158,35 @@ export default function Canvas({
       }
     }
 
+    console.log("spot", spot);
+    console.log("farmInfo", farmInfo);
+
     if (
       !!spot &&
-      (spot.parent == app.blinkingItem ||
-        (app.blinkingItem != null && app.blinkingItem.parent == spot.parent))
+      (spot.parent === app.blinkingItem ||
+        (app.blinkingItem !== null && app.blinkingItem.parent === spot.parent))
     ) {
+      console.log("spot 클릭으로 진행");
+
       onClickSpot(spot);
+
+      console.log("onClickSpot함수 완료");
+
       setNowSpot(spot.farmInfo.id);
     } else if (!!sector) {
+      console.log("sector 클릭으로 진행");
+
       onClickSector(sector);
+      lastSector = sector.sectorId;
     }
-    // console.log(sectorId, sector, spot, farmInfo);
   });
 
-  //.decelerate()
   // add the viewport to the stage
   app.stage.addChild(viewport);
 
   // Attach background
   const background = viewport.addChild(PIXI.Sprite.from("/Map/sector_0.png"));
-  if (isMobile == false) {
+  if (isMobile === false) {
     app.cloudsVfx = makeShader();
     app.cloudsVfx.scale.set(
       (BACKGROUND_SIZE.width * 3) / app.cloudsVfx.originalWidth,
@@ -161,23 +196,17 @@ export default function Canvas({
     app.cloudsVfx.x = -BACKGROUND_SIZE.width;
     app.cloudsVfx.y = -BACKGROUND_SIZE.height;
   }
+  if (app.cloudsVfx) background.addChild(app.cloudsVfx);
 
   const uiLayer = new PIXI.Container();
   app.stage.addChild(uiLayer);
-
-  // Attach spots
-  let buttons = new PIXI.Container();
-  let spotDict = {};
-  let sectorSpotDict = {};
-  let sectorDict = {};
-  let lastPopup = null;
 
   items.forEach((e, i, a) => {
     let farmInfo = e;
     let spot_x = farmInfo.x * 5.6 + 71;
     let spot_y = farmInfo.y * 5.6 + 92;
     let sector = makeSector(spot_x, spot_y, buttons);
-    if (sector.sectorId in sectorSpotDict == false)
+    if (sector.sectorId in sectorSpotDict === false)
       sectorSpotDict[sector.sectorId] = [];
 
     let spot = makeSpot(farmInfo, spot_x, spot_y, sector, sector.sectorId);
@@ -187,7 +216,6 @@ export default function Canvas({
   });
 
   background.addChild(buttons);
-  if (app.cloudsVfx) background.addChild(app.cloudsVfx);
 
   let elapsed = 0.0;
   app.ticker.add((delta) => {
@@ -198,8 +226,6 @@ export default function Canvas({
       let b = lastPopup.followTarget.getBounds();
       lastPopup.x = b.x + b.width / 2;
       lastPopup.y = b.y + b.height / 2;
-
-      // console.log("Ticker 구문 lastPopup", lastPopup); //! 계속 출력되므로 켜지 말것
     }
 
     if (app.blinkingItem) {
@@ -215,23 +241,21 @@ export default function Canvas({
     }
   });
 
-  function onClickGoButton(id) {
-    console.log(id);
-
-    if (id in spotDict) {
-      removePopup();
-
-      setNowSpot(id);
-
-      onClickSpot(spotDict[id]);
+  function mountCanvas(app, targetRef) {
+    if (targetRef.current) {
+      return targetRef.current.appendChild(app.view);
     }
+  }
+
+  function logApp() {
+    console.log(app);
   }
 
   function makeSector(spot_x, spot_y, parent) {
     let sectorId =
       Math.floor(spot_x / SECTOR_SIZE) + "_" + Math.floor(spot_y / SECTOR_SIZE);
     let sector = null;
-    if (sectorId in sectorDict == false) {
+    if (sectorId in sectorDict === false) {
       sector = new PIXI.Container();
 
       let lockedCover = new PIXI.Graphics();
@@ -256,20 +280,11 @@ export default function Canvas({
       sector.interactive = true;
       sector.buttonMode = true;
 
-      if (sectorId in UNLOCKED_SECTOR == false) {
+      if (sectorId in UNLOCKED_SECTOR === false) {
         const locked = PIXI.Sprite.from("/Map/locked.png");
         locked.anchor.set(0.5, 0.5);
         sector.addChild(locked);
       }
-
-      // let label = new PIXI.Text(sectorId,{fontFamily : 'Arial', fontSize: 25, fill : 0xffFFff, align : 'center'});
-      // label.x = -30-15
-      // label.y = -30-15
-      // let label_shadow = new PIXI.Text(sectorId,{fontFamily : 'Arial', fontSize: 25, fill : 0x000000, align : 'center'});
-      // label_shadow.x = -28-15
-      // label_shadow.y = -28-15
-      // sector.addChild(label_shadow);
-      // sector.addChild(label);
 
       parent.addChild(sector);
     } else {
@@ -282,9 +297,9 @@ export default function Canvas({
   function makeSpot(farmInfo, spot_x, spot_y, parent, sectorId) {
     let spot = new PIXI.Graphics();
     const rectSize = 5;
-    if (farmInfo.size == "5X5") spot.beginFill(0x774466, 0.5);
-    else if (farmInfo.size == "6X6") spot.beginFill(0x665588, 0.5);
-    else if (farmInfo.size == "7X7") spot.beginFill(0x3355bb, 0.5);
+    if (farmInfo.size === "5X5") spot.beginFill(0x774466, 0.5);
+    else if (farmInfo.size === "6X6") spot.beginFill(0x665588, 0.5);
+    else if (farmInfo.size === "7X7") spot.beginFill(0x3355bb, 0.5);
 
     spot.drawRect(-rectSize / 2, -rectSize / 2, rectSize, rectSize);
     spot.endFill();
@@ -303,6 +318,7 @@ export default function Canvas({
   function onClickSector(sector) {
     removePopup();
     setBlinkingTarget(sector);
+
     viewport.snap(sector.x, sector.y, {
       time: 300,
       removeOnComplete: true,
@@ -313,13 +329,13 @@ export default function Canvas({
   }
 
   function onClickSpot(spot) {
-    // console.log("spot/spotDict", spotDict);
-    // console.log("spot/sectorSpotDict", sectorSpotDict);
-    // console.log("spot/sectorDict", sectorDict);
-
+    console.log("onClickSpot함수 진행 -------------------");
     console.log("클릭 대상", spot);
 
     setBlinkingTarget(spot);
+
+    console.log("빤짝이함수 완료");
+
     viewport.snap(spot.parent.x + spot.x, spot.parent.y + spot.y, {
       time: 300,
       removeOnComplete: true,
@@ -330,13 +346,15 @@ export default function Canvas({
   function removePopup() {
     console.log("이전 이벤트 대상", lastPopup);
 
-    if (lastPopup != null) {
+    if (lastPopup !== null) {
       lastPopup.parent.removeChild(lastPopup);
       lastPopup = null;
     }
   }
 
   function showInfoPopup(spot) {
+    console.log("showInfoPopup 함수 진행----------------");
+    console.log("팝업 정보", spot.farmInfo);
     let farmInfo = spot.farmInfo;
 
     removePopup();
@@ -353,6 +371,8 @@ export default function Canvas({
       setDetailData(farmInfo);
       setSpotModal(true);
     });
+
+    console.log("팝업 생성 완료", popup);
 
     // set title text
     let popupTitleText = new PIXI.Text("[2321] 333.333", {
@@ -395,6 +415,8 @@ export default function Canvas({
     popupSizeText.x = 20;
     popupSizeText.text = farmInfo.size;
 
+    console.log("팝업 내용물 생성 완료", popupTitleText);
+
     //attach
     popupBg.anchor.set(0.5, 1);
     popupBg.addChild(moreBtn);
@@ -408,44 +430,43 @@ export default function Canvas({
     popup.followTarget = spot;
     uiLayer.addChild(popup);
 
-    // console.log("생성된 Popup", popup);
-    // console.log("uiLayer", uiLayer);
+    console.log("uiLayer에 팝업 추가 완료", uiLayer);
 
     lastPopup = popup;
 
     // show selected row
-    updateSelectedRow(farmInfo.id);
+    // updateSelectedRow(farmInfo.id);
   }
 
-  var blinkingGizmo = new PIXI.Graphics();
-  blinkingGizmo.zOrder = 10000;
-  blinkingGizmo.interactive = false;
-  blinkingGizmo.buttonMode = false;
   function setBlinkingTarget(target) {
-    // console.log("setBlinkingTarget", target);
+    console.log("setBlinkingTarget 함수 진행 ------------------------------");
+    console.log("빤짝이 타겟", target);
 
-    blinkingGizmo.clear();
+    blinkingGizmo.clear(); //! 이거 효과 있었음 지우지 말것
 
     if ("farmInfo" in target) {
+      console.log("Spot감지: 정보 팝업 생성");
       showInfoPopup(target);
-      updateSelectedRow(target.farmInfo.id);
     }
 
-    for (let sectorId in sectorDict) {
-      //last item was sector
-      let sector = sectorDict[sectorId];
+    //! 개선(?): 데이터 처리를 줄인 것 같은데 왜 더 느려지는...?
+    if (lastSector) {
+      const sector = sectorDict[lastSector];
       sector.parent.setChildIndex(sector, 0);
-      let spots = sectorSpotDict[sectorId];
+
+      const spots = sectorSpotDict[lastSector];
 
       spots.forEach((e) => {
         e.visible = false;
       });
-      // console.log("hide last spots");
     }
 
-    if (target != null) {
+    if (target !== null) {
       app.blinkingItem = target;
       let bound = app.blinkingItem.getLocalBounds();
+
+      console.log("bound", bound); // 여기까지도 제대로 나옴
+
       blinkingGizmo.lineStyle({
         width: 3,
         color: 0xffdd00,
@@ -453,6 +474,10 @@ export default function Canvas({
         join: PIXI.LINE_JOIN.ROUND,
       });
       blinkingGizmo.drawRect(bound.x, bound.y, bound.width, bound.height);
+      blinkingGizmo.endFill();
+
+      console.log("blinkingGizmo", blinkingGizmo);
+      console.log("다른 곳에 데이터가 들어가나?", app);
 
       app.blinkingItem.addChild(blinkingGizmo);
 
@@ -472,66 +497,24 @@ export default function Canvas({
           e.visible = true;
         });
         let sector = sectorDict[target.sectorId];
-        sector.parent.setChildIndex(sector, sector.parent.children.length - 1); // sectorDict[target.sectorId].zOrder = 100;
+        sector.parent.setChildIndex(sector, sector.parent.children.length - 1);
       }
-    }
-
-    if (target != null) {
-      if ("farmInfo" in target) {
-        let sector = sectorDict[target.parentSectorId];
-        showFarmInfosInSector(sector);
-      } else if ("sectorId" in target) {
-        let sector = sectorDict[target.sectorId];
-        showFarmInfosInSector(sector);
-      }
-    } else {
-      showFarmInfosInSector(null);
     }
   }
 
-  var lastInspectorShowSector = null;
-  function showFarmInfosInSector(sector, cb) {
-    // if (sector != null) {
-    //   $("#sector-inspector").show();
-    //   if (lastInspectorShowSector != sector) {
-    //     $("#pastures-list").scrollTop(0);
-    //     $("#pastures-list").empty();
-    //     fetch(window.location.origin + `/map/sector/${sector.sectorId}`) // Fetch sold states
-    //       .then((resp) => resp.json())
-    //       .then((data) => {
-    //         window.soldStateDict = {};
-    //         data.forEach((e) => {
-    //           window.soldStateDict[e.id] = e.sold; //0=false 1=true
-    //         });
-    //         updateInfoInSector(sector.sectorId);
-    //         if (app.blinkingItem != null && "farmInfo" in app.blinkingItem)
-    //           updateSelectedRow(app.blinkingItem.farmInfo.id);
-    //         cb && cb();
-    //       });
-    //   }
-    //   lastInspectorShowSector = sector;
-    // } else {
-    //   $("#sector-inspector").hide();
-    // }
-  }
+  function onClickGoButton(id) {
+    console.log("--------------------------------------------------------");
+    console.log("사이드바 클릭 이벤트");
+    console.log("App 체크", app);
+    console.log("클릭한 대상 id", id);
 
-  function updateSelectedRow(farmId) {
-    // $(`div.row[data-farm-id!="${farmId}"]`).removeClass("selected");
-    // $(`div.row[data-farm-id="${farmId}"]`).addClass("selected");
-    // if ($(`div.row[data-farm-id=${farmId}]`).length > 0) {
-    //   let row_position = $(`div.row[data-farm-id=${farmId}]`).offset().top;
-    //   let listTop = $("#pastures-list").offset().top;
-    //   let listBottom =
-    //     $("#pastures-list").offset().top + $("#pastures-list").height();
-    //   if (row_position < listTop || row_position > listBottom) {
-    //     $("#pastures-list").animate(
-    //       {
-    //         scrollTop: row_position - $("div.row").offset().top,
-    //       },
-    //       300
-    //     );
-    //   }
-    // }
+    if (id in spotDict) {
+      removePopup();
+
+      setNowSpot(id);
+
+      onClickSpot(spotDict[id]);
+    }
   }
 
   function onClickLandSearch() {
@@ -544,6 +527,7 @@ export default function Canvas({
 
   function onClickWelcomeModalOk() {
     // $("#welcom-modal-background").hide();
+    lastSector = FIRST_FOCUS_SECTOR;
     onClickSector(sectorDict[FIRST_FOCUS_SECTOR]);
   }
 
@@ -554,7 +538,7 @@ export default function Canvas({
 
   return (
     <Main>
-      <div ref={canvasParent} />
+      <div ref={canvasParent} id="test" />
       <WelcomeModal
         welcomeModal={welcomeModal}
         setWelcomeModal={handleWelcomeModal}
@@ -571,19 +555,52 @@ export default function Canvas({
         onClickGoButton={onClickGoButton}
       />
       <Search onClickLandSearch={onClickLandSearch} />
-      {/* <TEST onClick={() => console.log("dd")}>테스트버튼</TEST> */}
+      <TEST onClick={logApp}>테스트버튼</TEST>
     </Main>
   );
 }
 
 /*
 
-원래 계획: Pasture과 같이 움직이게 하려고 클릭시 id를 상태값으로 저장해 이벤트를 보여주려고 했는데
-  useEffect안에서 호출한 setBlinkingTarget 함수는 sectorSpotDict를 제대로 가져오지 못함(빈 객체)
+! 아니 왜 똑같은 함수인데 작동을 안하냐고 콘솔로그도 다 찍히는데 진짜 뭘 어떻게 해달라는거야...
 
-수정 계획: sectorSpotDict를 상태값으로 저장해보기
+- 성공한 것: 상태값으로 안 쓰니까 찾는 값들이 변수에 잘 잡힘
+- 안되는 것: 근데 왜 함수실행은 다 되는데 결과를 안보여주는 것이지?
+
+가설
+  1. 중간에 조건 타서 실행되지 않고 빠지는 함수가 있나?
+    - 원래 클릭에 따라 실행되는 함수들에 로그 찍어서 제대로 된 데이터가 나오는지 체크해보기
+    !> 똑같은함수 타고 로그 다 찍히는데???
+
+  2. 그럼 먼저 onClickSector를 적용해보기
+    - WelcomeModal 꺼질때의 onClickSector 적용을 다른 곳으로 바꿔보기
+    !> 적용 잘됨
+    
+    - onClickSpot대신 다른 섹터 포커싱 줘보기
+    !> ?? 왜 사이드바만 바뀌냐고
+    !> 적용안되는게 그래픽 쪽만 안되는듯...
+    !> 그러면 지금 함수실행은 되는데 그래픽 적용이 안되네?
+
+  //2. useEffect로 app을 적용해서 적용이 제대로 안되나?
+
+  //3. Sidebar에서 클릭하면 데이터 적용이 다른 곳으로 되나?
+
+  !상태: 함수 실행은 되는데 Graphics 적용이 제대로 안됨.
+    !> 증명: onClickGoButton함수에 onClickSector 할당하면 사이드바만 바뀜.
+    !> 값 로그찍어보면 분명 로그는 다 바뀌지만 따로 button으로 app로그찍어보면 blinkingItem이 없음
+      이것도 useEffect때문인가?
+      근데 useEffect안쓰면 초기값이 null이라 100% 오류나는데?
+
+  !원인: 이제 찾아야지
+    - 추정1: useRef가 문제인가?
+      useRef는 DOM에 연결하여 해당 Element를 읽어오거나, 
+      .current 프로퍼티에 값을 저장해놓고 해당 값이 변동되어도 렌더링하고 싶지 않을 때 사용?
+      일단... div는 빈 요소이고 모달 뜰때까지도 undefined인데 모달 종료되면 useEffect가 돌면서 추가되지 않나?
 
 
-! Pasture클릭을 함수로 줘서 
+  !해결: 이제 찾아야지
 
 */
+// console.log("spot/spotDict", spotDict);
+// console.log("spot/sectorSpotDict", sectorSpotDict);
+// console.log("spot/sectorDict", sectorDict);
